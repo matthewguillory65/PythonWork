be51c06bf1cbb1b80407cfb66894b49c8d50a999
matthewguillory65 <matthewguillory65@gmail.com>
2017-03-10 12:00:56 -0600

    Morning Assignment
    Morning Assignment

---
+++ PythonWork/pythonwork.py
@@ -0,0 +1,43 @@
+import random
+
+class Player(object):
+    def __init__(self, name):
+        self._name = name
+        self._card = []
+
+    def add_cards(self, cards):
+        self._card.append(cards)
+        return cards
+
+
+
+Thecards = [['Light', 5], ['Dark', 10]]
+
+a = Player('Matthew')
+a._name
+a.add_cards(random.choice(Thecards))
+a.add_cards(random.choice(Thecards))
+a.add_cards(random.choice(Thecards))
+a.add_cards(random.choice(Thecards))
+a.add_cards(random.choice(Thecards))
+
+b = Player('Zyron')
+b._name
+b.add_cards(random.choice(Thecards))
+b.add_cards(random.choice(Thecards))
+b.add_cards(random.choice(Thecards))
+b.add_cards(random.choice(Thecards))
+b.add_cards(random.choice(Thecards))
+
+def HigherPoints(player1, player2):
+    guy = 0
+    guy2 = 0
+
+    for x in range(4):
+        guy = guy + player1._card[x][1]
+    for y in range(4):
+        guy2 = guy2 + player2._card[y][1]
+    
+    return [player1, guy] if guy > guy2 else [player2, guy2]
+
+print HigherPoints(a, b)
